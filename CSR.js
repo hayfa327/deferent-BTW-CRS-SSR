17a7e29b505bae8ea0317b2f4157118db855ed9a
hayfa safa <haifasafaa@gmail.com>
2026-02-23 18:27:56 +0100

    createElement section

--- CSR.js
+++ CSR.js
@@ -4,3 +4,16 @@ const sections = [
   {id: "tips", title: "Tips & Guides"}, 
   {id: "hotTopic", title: "Hot Topic"}
 ]; 
+
+const app = document.getElementById("app"); 
+
+sections.forEach(section => {
+  const sectionElement = document.createElement("section");
+
+  sectionElement.innerHTML = `
+  <h2>${section.title}</h2>;
+  <div id="${section.id}-container"></div>;
+  `
+
+  app.appendChild(sectionElement);
+});
