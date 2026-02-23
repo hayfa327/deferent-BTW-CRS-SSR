19a6cc6f73058eab3b66d39caffb24d0a1f07b70
hayfa safa <haifasafaa@gmail.com>
2026-02-23 18:14:36 +0100

    add array for the sections

---
+++ CSR.js
@@ -0,0 +1,6 @@
+const sections = [
+  {id: "latest", title:"Latest News"}, 
+  {id: "trending", title:"Trending"}, 
+  {id: "tips", title: "Tips & Guides"}, 
+  {id: "hotTopic", title: "Hot Topic"}
+]; 
