985e08991a2a40eb7e02f9725c4b2b8478a58fa7
hayfa safa <haifasafaa@gmail.com>
2026-02-23 18:38:09 +0100

    add latestNews array

--- CSR.js
+++ CSR.js
@@ -17,3 +17,28 @@ sections.forEach(section => {
 
   app.appendChild(sectionElement);
 });
+
+
+const LatestNews = [
+  {
+    Image: "./assets/latest-news/news-one.png", 
+    title: "Elden Ring 2 Announced",
+    description: "Epic fantasy adventure sequel unveiled, promising a darker world and vast exploration."  
+  }, 
+ {
+  Image: "./assets/latest-news/news-two.png", 
+    title: "Cyberpunk 2077 Expansion",
+    description: "The sequel expands the universe with deeper storytelling, improved and a vast open world to explore."  
+  }, 
+ {
+  Image: "./assets/latest-news/news-three.png", 
+    title: "PSS Pro Rumors Heat Up",
+    description: "Rumors are swirling about Sony's next-generation console, with speculation about enhanced performance."  
+  }, 
+   {
+  Image: "./assets/latest-news/news-four.png", 
+    title: "Top RPGs to Watch in 2026",
+    description: "Exciting new role-playing games are set to release this year, offering fresh stories."  
+  }, 
+
+]
