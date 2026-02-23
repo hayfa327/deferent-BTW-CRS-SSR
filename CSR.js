aed006aad87bef76cde3b5582644f84dedf48d45
hayfa safa <haifasafaa@gmail.com>
2026-02-23 19:04:09 +0100

    add the arrys for all the functions

--- CSR.js
+++ CSR.js
@@ -11,7 +11,7 @@ sections.forEach(section => {
   const sectionElement = document.createElement("section");
 
   sectionElement.innerHTML = `
-  <h2>${section.title}</h2>;
+  <h2 class="titleSectionTwo">${section.title}</h2>;
   <div id="${section.id}-container"></div>;
   `
 
@@ -19,7 +19,7 @@ sections.forEach(section => {
 });
 
 
-const LatestNews = [
+const latest= [
   {
     Image: "./assets/latest-news/news-one.png", 
     title: "Elden Ring 2 Announced",
@@ -41,4 +41,72 @@ const LatestNews = [
     description: "Exciting new role-playing games are set to release this year, offering fresh stories."  
   }, 
 
-]
+];
+
+
+const trending = [
+  {
+    Image: "./assets/trending/trending-one.jpg", 
+    title: "COD: Warzone Season 5",
+    description: "Announced at the 2026 E3 conference, Warzone Season 5 brings new maps"  
+  }, 
+ {
+  Image: "./assets/trending/trending-two.png", 
+    title: "Zelda's New Adventure",
+    description: "DLC revealed with new characters, locations, and gameplay mechanics."  
+  }, 
+ {
+  Image: "./assets/trending/trending-three.png", 
+    title: "Epic Indie Games Surprise",
+    description: "Discover the most innovative and critically acclaimed indie games of 2026."  
+  }
+];
+
+
+
+const tips = [
+ {
+  Image: "./assets/trending/trending-two.png", 
+    title: "Top FPS Tips for Better Aim",
+    description: "Improve your accuracy in first-person shooters with these essential tips and tricks."  
+  }, 
+ {
+  Image: "./assets/trending/trending-three.png", 
+    title: "Level Up Your RPG Skills",
+    description: "Learn how to improve your character's abilities and progression in role-playing games."  
+  }
+]; 
+
+const hotTopics = [
+  { title: "Action" },
+  { title: "RPG" },
+   { title: "Shooter" },
+    { title: "Multiplayer" },
+     { title: "Adventure" }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+ function createCard(item) {
+  const card = document.createElement("div");
+  card.classList.add("news-card");
+
+  card.innerHTML = `
+    ${item.image ? `<img class="eldePic" src="${item.image}" alt="${item.title}">` : ""}
+    <div class="content-news">
+      <h3>${item.title}</h3>
+      <p>${item.description || ""}</p>
+    </div>
+  `;
+
+  return card;
+}
