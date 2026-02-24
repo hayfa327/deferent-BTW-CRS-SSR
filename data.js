2e3af79d24d258a92b93af327f9f60f338bc2e47
hayfa safa <haifasafaa@gmail.com>
2026-02-24 11:15:18 +0100

    add all the change

--- data.js
+++ data.js
@@ -1,9 +1,3 @@
-export const sections = [
-  {id: "latest", title:"Latest News"}, 
-  {id: "trending", title:"Trending"}, 
-  {id: "tips", title: "Tips & Guides"}, 
-  {id: "hotTopic", title: "Hot Topic"}
-]; 
 
 export const latest= [
   {
